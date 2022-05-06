afc4d76f93c3237245aa4eca9c490db4ad4cf1f1
DnelZpt <daniel.zapata1@utp.edu.co>
2022-05-06 18:06:44 -0500

    spaces corrected

--- daqthreephi.py
+++ daqthreephi.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import sawtooth, square
 
+
 class DataGen():
 
     def __init__(self, seed):
         F = 60
-        Ts = 1/(100*F)
-        N = int(5/(F*Ts))
-        t = np.linspace(0.0, N*Ts, N, endpoint=False)
+        Ts = 1 / (100 * F)
+        N = int(5 / (F * Ts))
+        t = np.linspace(0.0, N * Ts, N, endpoint=False)
         self.rs = np.random.RandomState(seed)
         self.__voltages = self.__get_voltages(F, t)
         self.__currents = self.__get_currents(F, t)
@@ -28,12 +29,11 @@ class DataGen():
         for ax, col in zip(axs[0], col_labels):
             ax.set_title(col)
 
-        for ax, row in zip(axs[:,0], row_labels):
+        for ax, row in zip(axs[:, 0], row_labels):
             ax.set_ylabel(row, rotation=0, size='large')
 
         fig.tight_layout()
         plt.show()
-
 
     def __hrect(self, t, *args):
         fun = np.heaviside(np.sin(t), 1) * np.sin(t)
@@ -47,10 +47,10 @@ class DataGen():
         v = list([0, 0, 0])
         n_harmonics = 5
         amplitude = [0, 220, 0, 20, 0, 8]
-        for k, amp in zip(range(n_harmonics+1), amplitude):
-            v[0] += amp * np.sin(k*(2*np.pi*F*t))
-            v[1] += amp * np.sin(k*(2*np.pi*F*t+240*np.pi/180))
-            v[2] += amp * np.sin(k*(2*np.pi*F*t+120*np.pi/180))
+        for k, amp in zip(range(n_harmonics + 1), amplitude):
+            v[0] += amp * np.sin(k * (2 * np.pi * F * t))
+            v[1] += amp * np.sin(k * (2 * np.pi * F * t + 240 * np.pi / 180))
+            v[2] += amp * np.sin(k * (2 * np.pi * F * t + 120 * np.pi / 180))
         return v
 
     def __get_currents(self, F, t):
@@ -59,8 +59,8 @@ class DataGen():
         i = list([0, 0, 0])
         d = self.rs.rand()
         a = 220 * d
-        i[0] = a * func(2*np.pi*F*t, d)
-        i[1] = a * func(2*np.pi*F*t+240*np.pi/180, d)
-        i[2] = a * func(2*np.pi*F*t+120*np.pi/180, d)
+        i[0] = a * func(2 * np.pi * F * t, d)
+        i[1] = a * func(2 * np.pi * F * t + 240 * np.pi / 180, d)
+        i[2] = a * func(2 * np.pi * F * t + 120 * np.pi / 180, d)
 
         return i
